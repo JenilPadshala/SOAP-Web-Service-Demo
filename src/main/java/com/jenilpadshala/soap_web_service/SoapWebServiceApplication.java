88c1924ff331c79de1504a7c09de7b6eb55b2299
Jenil Padshala <jenilpadshala@gmail.com>
2024-08-27 18:53:19 +0530

    added all files

---
+++ src/main/java/com/jenilpadshala/soap_web_service/SoapWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jenilpadshala.soap_web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapWebServiceApplication.class, args);
+	}
+
+}
